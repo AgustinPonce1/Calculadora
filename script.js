3bb1e720d4c37a2a7275329d020de9ad53db490f
AgustinPonce1 <136716814+AgustinPonce1@users.noreply.github.com>
2023-09-10 15:01:50 -0300

    v1.0.0

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let dolar_oficial = 300;
+let dolar_oficial = 367;
 
 // Aluminio 75x25 - 1mt vale 12.36 dólar
-let aluminioAnodizado_75x25 = 12.36;
+let aluminioAnodizado_75x25 = 15.30;
 let pedido_75x25 = parseFloat(prompt("Metros de Aluminio Anodizado 75x25 deseados: "));
 
 let metros_75x25 = pedido_75x25;
@@ -9,7 +9,7 @@ let precio_75x25 = aluminioAnodizado_75x25 * metros_75x25;
 console.log("Precio Total para " + pedido_75x25 + " metros de aluminio 75x25 = " + precio_75x25 + "\n");
 
 // Aluminio 75x25 - 1mt vale 8.66 dólar
-let aluminioAnodizado_50x25 = 8.66;
+let aluminioAnodizado_50x25 = 10.50;
 let pedido_50x25 = parseFloat(prompt("Metros de Aluminio Anodizado 50x25 deseados: "));
 
 let metros_50x25 = pedido_50x25;
@@ -17,7 +17,7 @@ let precio_50x25 = aluminioAnodizado_50x25 * metros_50x25;
 console.log("Precio Total para " + pedido_50x25 + " metros de aluminio 50x25 = " + precio_50x25 + "\n");
 
 // Almohadones Grande y Chico - combo almohadones por sillon de un cuerpo vale 80 dólar
-let almohadon_grandeYchico = 80;
+let almohadon_grandeYchico = 108;
 let pedido_almohadones = parseFloat(prompt("Combos de almohadones deseados: "));
 
 let metros_almohadones = pedido_almohadones;
